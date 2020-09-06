465c1449142387d71ac4d97c5678dd8124f620d7
Brasidas00 <guo_peichen@163.com>
2020-09-07 01:59:20 +0800

    1.2 列表check完成,重启应用后仍可显示灰色,DatabaseServer添加清空新闻浏览记录的功能

--- app/src/main/java/com/java/chenxin/background/DatabaseServer.java
+++ app/src/main/java/com/java/chenxin/background/DatabaseServer.java
@@ -1,5 +1,9 @@
 package com.java.chenxin.background;
 
-public class DatabaseServer {
+import com.java.chenxin.data_struct.NewsPiece;
 
+public class DatabaseServer {
+    public static void clearNewsHistory(){//清空新闻数据
+        NewsPiece.deleteAll(NewsPiece.class);
+    }
 }
