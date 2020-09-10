89c3e3953e21aa1bd16b0aba17f8812557fb983e
OldNew777 <1243705875@qq.com>
2020-09-11 03:20:17 +0800

    1.35
    疫情图谱
        完成一个relation连接；
        搜“药物”闪退

--- app/src/main/java/com/java/chenxin/ui/data/epidemicMap/EntityActivity.java
+++ app/src/main/java/com/java/chenxin/ui/data/epidemicMap/EntityActivity.java
@@ -1,9 +1,11 @@
 package com.java.chenxin.ui.data.epidemicMap;
 
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.util.AttributeSet;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
@@ -15,11 +17,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import com.bumptech.glide.Glide;
 import com.java.chenxin.R;
 import com.java.chenxin.data_struct.Entity;
+import com.java.chenxin.data_struct.Relation;
+import com.java.chenxin.ui.news.search.SearchActivity;
 
+import java.util.List;
 import java.util.zip.Inflater;
 
 public class EntityActivity extends AppCompatActivity {
     private Entity entity;
+    private List<Relation> relationList;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -32,16 +38,32 @@ public class EntityActivity extends AppCompatActivity {
         ImageView image_view = (ImageView) findViewById(R.id.image);
         ListView relationListView = (ListView) findViewById(R.id.relations_listview);
 
+        // 获取数据
         entity = (Entity) getIntent().getSerializableExtra("Entity");
 
+        // 设置页面
         setTitle(getTitle() + " : " + entity.getLabel());
         title_textView.setText(entity.getLabel());
         description_textView.setText(entity.getDiscription());
         Glide.with(this).load(entity.getImgUrl()).into(image_view);
 
-        RelationAdapter relationAdapter = new RelationAdapter(this, R.layout.item_relation,
-                entity.getRelationList());
+        relationList = entity.getRelationList();
+        RelationAdapter relationAdapter = new RelationAdapter(this, R.layout.item_relation, relationList);
         relationListView.setAdapter(relationAdapter);
+
+        // 监听relation连接
+        relationListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long column) {
+                String query = relationList.get(position).getLabel();
+                Intent returnIntent = new Intent(EntityActivity.this, EpidemicMapFragment.class);
+
+                returnIntent.putExtra("SearchQuery", query);
+                setResult(0, returnIntent);
+
+                EntityActivity.this.finish();
+            }
+        });
     }
 
 }
