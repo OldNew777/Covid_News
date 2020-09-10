1bddbd1881cd0ce8f359ccdbb26567298067e91f
Brasidas00 <guo_peichen@163.com>
2020-09-10 21:56:03 +0800

    1.35 聚类可以显示了

---
+++ app/src/main/java/com/java/chenxin/ui/newsClustering/EventListAdapter.java
@@ -0,0 +1,48 @@
+package com.java.chenxin.ui.newsClustering;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+
+import com.java.chenxin.R;
+import com.java.chenxin.data_struct.NewsPiece;
+
+import java.util.List;
+
+public class EventListAdapter extends ArrayAdapter<NewsPiece> {
+    private int resourceId;
+
+    public EventListAdapter(@NonNull Context context, int resource, @NonNull List<NewsPiece> objects) {
+        super(context, resource, objects);
+        resourceId = resource;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @NonNull View convertView, @NonNull ViewGroup parent){
+        NewsPiece newsPiece = getItem(position);
+        View view = LayoutInflater.from(getContext()).inflate(resourceId, parent, false);
+
+        TextView title = (TextView)view.findViewById(R.id.event_list_title);
+        TextView source = (TextView)view.findViewById(R.id.event_list_source);
+        TextView data = (TextView)view.findViewById(R.id.event_list_date);
+
+        title.setText(newsPiece.getTitle());
+        source.setText(newsPiece.getSource());
+        data.setText(newsPiece.getDate());
+//        if (newsPiece.getIsRead()){
+//            title.setTextColor(Color.GRAY);
+//        }
+//        else{
+//            title.setTextColor(Color.BLACK);
+//        }
+
+        return view;
+    }
+}
--- app/src/main/java/com/java/chenxin/ui/newsClustering/NewsClusteringFragment.java
+++ app/src/main/java/com/java/chenxin/ui/newsClustering/NewsClusteringFragment.java
@@ -1,5 +1,6 @@
 package com.java.chenxin.ui.newsClustering;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -8,15 +9,50 @@ import android.view.ViewGroup;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.viewpager.widget.ViewPager;
 
 import com.java.chenxin.R;
+import com.java.chenxin.universal.FragmentAdapter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class NewsClusteringFragment extends Fragment {
+    private EventListFragment medicineFragment;
+    private EventListFragment virusFragment;
+    private EventListFragment scientificFragment;
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        medicineFragment.onActivityResult(requestCode, resultCode, data);
+        virusFragment.onActivityResult(requestCode, resultCode, data);
+        scientificFragment.onActivityResult(requestCode, resultCode, data);
+    }
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
-        View root = inflater.inflate(R.layout.fragment_news_clustering, container, false);
+        View root = inflater.inflate(R.layout.fragment_viewpager, container, false);
+
+        List<Fragment> fragmentList = new ArrayList<>();
+        medicineFragment = new EventListFragment("medicine");
+        virusFragment = new EventListFragment("virus");
+        scientificFragment = new EventListFragment("scientific");
+        fragmentList.add(medicineFragment);
+        fragmentList.add(virusFragment);
+        fragmentList.add(scientificFragment);
+        List<String> titleList = new ArrayList<>();
+        titleList.add(getResources().getString(R.string.medicine));
+        titleList.add(getResources().getString(R.string.virus));
+        titleList.add(getResources().getString(R.string.scientific));
+
+        ViewPager viewPager = (ViewPager) root.findViewById(R.id.viewPager);
+        viewPager.setOffscreenPageLimit(fragmentList.size());
+        FragmentAdapter adapter = new FragmentAdapter(
+                getChildFragmentManager(), fragmentList, titleList);
+        viewPager.setAdapter(adapter);
 
         return root;
     }
